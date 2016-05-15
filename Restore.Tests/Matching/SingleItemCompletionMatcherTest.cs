2671f2766d10f9e16c1bc731a658612ba0b80d26
Ferry de Boer <ferry.de.boer@gmail.com>
2016-05-15 16:20:07 +0200

    adding new item to append on matches

---
+++ Restore.Tests/Matching/SingleItemCompletionMatcherTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using JetBrains.Annotations;
+using NUnit.Framework;
+using Restore.Matching;
+
+namespace Restore.Tests.Matching
+{
+    [TestFixture]
+    public class SingleItemCompletionMatcherTest
+    {
+        [SetUp]
+        public void SetUpTest()
+        {
+            var x = "test";
+        }
+
+        [Test]
+        public void ShouldTryMatchFromGivenSourceType()
+        {
+            // I need a complete configuration in order for this object to work. :o
+            var channelConfiguration = new TestConfiguration();
+            var _matcherUnderTest = new SingleItemCompletionMatcher<LocalTestResource, RemoteTestResource, int, ItemMatch<LocalTestResource, RemoteTestResource>>(
+                channelConfiguration,
+                typeof(LocalTestResource));
+
+            // Add resource that should be retrieved by matcher
+            channelConfiguration.Type1EndpointConfiguration.Endpoint.Create(new LocalTestResource(1, 10));
+
+            var testMatch = new ItemMatch<LocalTestResource, RemoteTestResource>(
+                null,
+                new RemoteTestResource(1, "Missing"));
+            _matcherUnderTest.Complete(testMatch);
+
+            Assert.IsNotNull(testMatch.Result1);
+        }
+    }
+
+    public class SingleItemCompletionMatcher<T1, T2, TId, TSynch>
+        where TId : IEquatable<TId>
+    {
+        private readonly IChannelConfiguration<T1, T2, TId, TSynch> _channelConfiguration;
+        private readonly Type _completionSourceType;
+
+        public SingleItemCompletionMatcher(IChannelConfiguration<T1, T2, TId, TSynch> channelConfiguration, Type completionSourceType)
+        {
+            _channelConfiguration = channelConfiguration;
+            _completionSourceType = completionSourceType;
+        }
+
+        public void Complete(ItemMatch<LocalTestResource, RemoteTestResource> itemMatch)
+        {
+            
+        }
+    }
+}
---
+++ Restore.Tests/TestConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Restore.ChangeResolution;
+using Restore.Channel.Configuration;
+using Restore.Matching;
+using Restore.Tests.ChangeResolution;
+
+namespace Restore.Tests
+{
+    public class TestConfiguration : IChannelConfiguration<LocalTestResource, RemoteTestResource, int, ItemMatch<LocalTestResource, RemoteTestResource>>
+    {
+        private readonly InMemoryCrudDataEndpoint<LocalTestResource, int> _t1Endpoint =
+            new InMemoryCrudDataEndpoint<LocalTestResource, int>(new TypeConfiguration<LocalTestResource, int>(ltr => ltr.CorrelationId ?? -1));
+
+        public IEndpointConfiguration<LocalTestResource, int> Type1EndpointConfiguration { get; }
+
+        private readonly InMemoryCrudDataEndpoint<RemoteTestResource, int> _t2Endpoint =
+            new InMemoryCrudDataEndpoint<RemoteTestResource, int>(new TypeConfiguration<RemoteTestResource, int>(rtr => rtr.Id));
+
+        public TestConfiguration()
+        {
+            Type2EndpointConfiguration = new EndpointConfiguration<RemoteTestResource, int>(_t2Endpoint.TypeConfig, _t2Endpoint);
+            Type1EndpointConfiguration = new EndpointConfiguration<LocalTestResource, int>(_t1Endpoint.TypeConfig, _t1Endpoint);
+            SynchronizationResolvers = new List<ISynchronizationResolver<ItemMatch<LocalTestResource, RemoteTestResource>>>();
+            TypeTranslator = new TestResourceTranslator();
+        }
+
+        public IEndpointConfiguration<RemoteTestResource, int> Type2EndpointConfiguration { get; }
+
+
+        public Func<IEnumerable<LocalTestResource>, IEnumerable<RemoteTestResource>, IEnumerable<ItemMatch<LocalTestResource, RemoteTestResource>>> ItemsPreprocessor
+        { get; set; }
+
+        public IEnumerable<ISynchronizationResolver<ItemMatch<LocalTestResource, RemoteTestResource>>> SynchronizationResolvers
+        { get; }
+
+        public ITranslator<LocalTestResource, RemoteTestResource> TypeTranslator { get; }
+    }
+}
