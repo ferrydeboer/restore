31eaa218c8f4705d87bee1908c43a8b008131b5c
Ferry de Boer <ferry.de.boer@gmail.com>
2016-06-15 13:39:41 +0200

    moved individual matcher into project

---
+++ Restore.Tests/Matching/IndividualItemMatcherTest.cs
@@ -0,0 +1,111 @@
+﻿using Restore.Channel.Configuration;
+using Restore.Matching;
+using Restore.Tests.ChangeResolution;
+
+namespace Restore.Tests.Matching
+{
+    [TestFixture]
+    public class IndividualItemMatcherTest
+    {
+        [NotNull]
+        private readonly IndividualItemMatcher<LocalTestResource, RemoteTestResource, int, ItemMatch<LocalTestResource, RemoteTestResource>> _itemMatcherUnderTest;
+        private List<LocalTestResource> _localResults;
+        private List<RemoteTestResource> _remoteResults;
+        private EndpointConfiguration<LocalTestResource, int> _t1EndpointCfg;
+        private EndpointConfiguration<RemoteTestResource, int> _t2EndpointCfg;
+        private IChannelConfiguration<LocalTestResource, RemoteTestResource, int, ItemMatch<LocalTestResource, RemoteTestResource>> _channelConfig;
+
+        public IndividualItemMatcherTest()
+        {
+            _t1EndpointCfg = CreateTestEndpointConfig<LocalTestResource, int>(ltr => ltr.CorrelationId ?? -1);
+            _t2EndpointCfg = CreateTestEndpointConfig<RemoteTestResource, int>(ltr => ltr.Id);
+
+            _channelConfig = new ChannelConfiguration<LocalTestResource, RemoteTestResource, int, ItemMatch<LocalTestResource, RemoteTestResource>>(
+                _t1EndpointCfg, _t2EndpointCfg, new TestResourceTranslator());
+
+            _itemMatcherUnderTest =
+                new IndividualItemMatcher<LocalTestResource, RemoteTestResource, int, ItemMatch<LocalTestResource, RemoteTestResource>>(
+                    _channelConfig, typeof(LocalTestResource));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            ((InMemoryCrudDataEndpoint<LocalTestResource, int>)_t1EndpointCfg.Endpoint).Clear();
+            ((InMemoryCrudDataEndpoint<RemoteTestResource, int>)_t2EndpointCfg.Endpoint).Clear();
+        }
+
+        public EndpointConfiguration<T, TId> CreateTestEndpointConfig<T, TId>(Func<T, TId> idResolver) where TId : IEquatable<TId>
+        {
+            var configuration = new TypeConfiguration<T, TId>(idResolver);
+            var endpoint = new InMemoryCrudDataEndpoint<T, TId>(configuration);
+            return new EndpointConfiguration<T, TId>(configuration, endpoint);
+        }
+
+        [Test]
+        public void ShouldReturnAlreadyCompleteMatches()
+        {
+            var itemMatch = new ItemMatch<LocalTestResource, RemoteTestResource>(
+                new LocalTestResource(1, 10),
+                new RemoteTestResource(1, "test"));
+
+            var result = _itemMatcherUnderTest.AppendIndividualItem(itemMatch);
+            Assert.AreEqual(itemMatch, result);
+        }
+
+        [Test]
+        public void ShouldAppendT1WhenDefaultButAvailable()
+        {            
+            var itemMatch = new ItemMatch<LocalTestResource, RemoteTestResource>(
+                null,
+                new RemoteTestResource(1, "test"));
+            var item1 = new LocalTestResource(1, 10);
+            _t1EndpointCfg.Endpoint.Create(item1);
+
+            var result = _itemMatcherUnderTest.AppendIndividualItem(itemMatch);
+
+            Assert.AreEqual(item1, result.Result1);
+            Assert.AreEqual(itemMatch.Result2, result.Result2);
+        }
+
+        [Test]
+        public void ShouldAppendT2WhenDefaultButAvailable()
+        {
+            
+            var itemMatch = new ItemMatch<LocalTestResource, RemoteTestResource>(
+                new LocalTestResource(1, 10),
+                null);
+            var item2 = new RemoteTestResource(1, "test");
+            _t2EndpointCfg.Endpoint.Create(item2);
+
+            var result = _itemMatcherUnderTest.AppendIndividualItem(itemMatch, typeof(RemoteTestResource));
+
+            Assert.AreEqual(item2, result.Result2);
+            Assert.AreEqual(itemMatch.Result1, result.Result1);
+        }
+
+        [Test]
+        public void ShouldReturnOriginalMatchWhenT1NotAvailable()
+        {
+            var itemMatch = new ItemMatch<LocalTestResource, RemoteTestResource>(
+                null,
+                new RemoteTestResource(1, "test"));
+
+            var result = _itemMatcherUnderTest.AppendIndividualItem(itemMatch);
+
+            Assert.AreEqual(itemMatch, result);
+        }
+
+        [Test]
+        public void ShouldReturnOriginalMatchWhenT2NotAvailable()
+        {
+            var itemMatch = new ItemMatch<LocalTestResource, RemoteTestResource>(
+                new LocalTestResource(1, 10),
+                null);
+
+            var result = _itemMatcherUnderTest.AppendIndividualItem(itemMatch, typeof(RemoteTestResource));
+
+            Assert.AreEqual(itemMatch, result);
+        }
+    }
+}
---
+++ Restore/Matching/IndividualItemMatcher.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace Restore.Matching
+{
+    /// <summary>
+    /// Used to append initial matchresults that is based on two lists. When the list does not contain
+    /// the items it can still exist (somewhere else) since the list is only the basis for the synchronization
+    /// this matcher queries the endpoint to possible append the match.
+    /// Could be written as a SynchronizationStep. But since it's added outside of channel
+    /// generation and doesn't transform types it's more in line with the matchers.
+    /// </summary>
+    public class IndividualItemMatcher<T1, T2, TId, TSynch>
+        where TId : IEquatable<TId>
+    {
+        private readonly Type _appendType;
+        public IChannelConfiguration<T1, T2, TId, TSynch> ChannelConfig { get; }
+
+        public IndividualItemMatcher([NotNull] IChannelConfiguration<T1, T2, TId, TSynch> channelConfig, Type appendType)
+        {
+            _appendType = appendType;
+            if (channelConfig == null) { throw new ArgumentNullException(nameof(channelConfig)); }
+            ChannelConfig = channelConfig;
+        }
+
+        public ItemMatch<T1, T2> AppendIndividualItem([NotNull] ItemMatch<T1, T2> initial, Type appendType = null)
+        {
+            if (appendType == null)
+            {
+                appendType = _appendType;
+            }
+
+            if (initial == null) { throw new ArgumentNullException(nameof(initial)); }
+
+            if(initial.IsComplete) { return initial; }
+
+            if (appendType == typeof(T1))
+            {
+                return Append<T1>(initial);
+            }
+            return Append<T2>(initial);
+        }
+
+        private ItemMatch<T1,T2> Append<T>(ItemMatch<T1, T2> match)
+        {
+            // Given the generics it very difficult to reduce duplication further here.
+            if (typeof(T) == typeof(T1) && EqualityComparer<T1>.Default.Equals(match.Result1, default(T1)))
+            {
+                var item1Id = ChannelConfig.Type2EndpointConfiguration.TypeConfig.IdExtractor(match.Result2);
+                var result1 = ChannelConfig.Type1EndpointConfiguration.Endpoint.Read(item1Id);
+                if (!EqualityComparer<T1>.Default.Equals(result1, default(T1)))
+                {
+                    return new ItemMatch<T1, T2>(result1, match.Result2);
+                }
+            }
+            if (typeof(T) == typeof(T2) && EqualityComparer<T2>.Default.Equals(match.Result2, default(T2)))
+            {
+                var itemId = ChannelConfig.Type1EndpointConfiguration.TypeConfig.IdExtractor(match.Result1);
+                var result2 = ChannelConfig.Type2EndpointConfiguration.Endpoint.Read(itemId);
+                if (!EqualityComparer<T2>.Default.Equals(result2, default(T2)))
+                {
+                    return new ItemMatch<T1, T2>(match.Result1, result2);
+                }
+            }
+
+            return match;
+        }
+
+        /*
+        public ItemMatch<T1, T2> AppendT1(ItemMatch<T1, T2> match, 
+            Func<ItemMatch<T1, T2>, T1> itemReader, 
+            Func<ItemMatch<T1, T2>, TId> idReader,
+            Func<ItemMatch<T1, T2>, T1> itemEndpointReader)
+        {
+            if (EqualityComparer<T1>.Default.Equals(itemReader(match), default(T1)))
+            {
+                var item1Id = idReader(match);
+                var result1 = itemEndpointReader(match);
+                if (EqualityComparer<T1>.Default.Equals(result1, default(T1)))
+                {
+                    return new ItemMatch<T1, T2>(result1, match.Result2);
+                }
+            }
+
+            return null;
+        }*/
+    }
+}
